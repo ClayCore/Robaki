f78aae4605f971779b7f2fcc4de489cbabae055b
lunarium <claymorealt@op.pl>
2023-07-29 19:05:05 +0200

    Add base event class for event management

---
+++ src/event/event.cpp
@@ -0,0 +1,91 @@
+#include "event.hpp"
+
+namespace event
+{
+    Event::Event()
+    {
+        m_type     = EventType::None;
+        m_category = EventCategory::None;
+    }
+
+    Event::Event(EventType const &type)
+    {
+        m_type     = type;
+        m_category = this->get_category_from_type(type);
+        m_name     = this->get_name_from_type(type);
+    }
+
+    auto Event::get_name_from_type(EventType const &type) const -> char const *
+    {
+        auto result = "";
+
+        // clang-format off
+        switch (type) {
+            case EventType::None:                   { result = "None";                }; break;
+            case EventType::WindowClose:            { result = "WindowClose";         }; break;
+            case EventType::WindowResize:           { result = "WindowResize";        }; break;
+            case EventType::WindowFocus:            { result = "WindowFocus";         }; break;
+            case EventType::WindowLostFocus:        { result = "WindowLostFocus";     }; break;
+            case EventType::WindowMoved:            { result = "WindowMoved";         }; break;
+            case EventType::LoadResource:           { result = "LoadResource";        }; break;
+            case EventType::FreeResource:           { result = "FreeResource";        }; break;
+            case EventType::EngineTick:             { result = "EngineTick";          }; break;
+            case EventType::EngineUpdate:           { result = "EngineUpdate";        }; break;
+            case EventType::EngineRender:           { result = "EngineRender";        }; break;
+            case EventType::KeyPressed:             { result = "KeyPressed";          }; break;
+            case EventType::KeyReleased:            { result = "KeyReleased";         }; break;
+            case EventType::MouseButtonPressed:     { result = "MouseButtonPressed";  }; break;
+            case EventType::MouseButtonReleased:    { result = "MouseButtonReleased"; }; break;
+            case EventType::MouseMoved:             { result = "MouseMoved";          }; break;
+            case EventType::MouseScrolled:          { result = "MouseScrolled";       }; break;
+            default:                                { result = "Unimplemented";       }; break;
+        }
+        // clang-format on
+
+        return (result);
+    }
+
+    auto Event::get_category_from_type(EventType const &type) const -> EventCategory
+    {
+        auto result = EventCategory::None;
+
+        // clang-format off
+        switch (type) {
+            case EventType::None:                   { result = EventCategory::None;       }; break;
+            case EventType::WindowClose:            { result = EventCategory::Window;     }; break;
+            case EventType::WindowResize:           { result = EventCategory::Window;     }; break;
+            case EventType::WindowFocus:            { result = EventCategory::Window;     }; break;
+            case EventType::WindowLostFocus:        { result = EventCategory::Window;     }; break;
+            case EventType::WindowMoved:            { result = EventCategory::Window;     }; break;
+            case EventType::LoadResource:           { result = EventCategory::Resource;   }; break;
+            case EventType::FreeResource:           { result = EventCategory::Resource;   }; break;
+            case EventType::EngineTick:             { result = EventCategory::Engine;     }; break;
+            case EventType::EngineUpdate:           { result = EventCategory::Engine;     }; break;
+            case EventType::EngineRender:           { result = EventCategory::Engine;     }; break;
+            case EventType::KeyPressed:             { result = EventCategory::Input;      }; break;
+            case EventType::KeyReleased:            { result = EventCategory::Input;      }; break;
+            case EventType::MouseButtonPressed:     { result = EventCategory::Input;      }; break;
+            case EventType::MouseButtonReleased:    { result = EventCategory::Input;      }; break;
+            case EventType::MouseMoved:             { result = EventCategory::Input;      }; break;
+            case EventType::MouseScrolled:          { result = EventCategory::Input;      }; break;
+            default:                                { result = EventCategory::Custom;     }; break;
+        }
+        // clang-format on
+
+        return (result);
+    }
+
+    auto Event::set_name(std::string const &name) -> void
+    {
+        m_name = name;
+    }
+
+    auto Event::to_string() const -> std::string
+    {
+        if (!m_name.empty()) {
+            return (m_name);
+        } else {
+            return (this->get_name_from_type(m_type));
+        }
+    }
+}  // namespace event
---
+++ src/event/event.hpp
@@ -0,0 +1,112 @@
+/** @file event.hpp */
+
+#pragma once
+
+// module includes
+#include "util/util.hpp"
+
+// fmt includes
+#include "fmt/format.h"
+
+namespace event
+{
+    enum class EventType
+    {
+        None = 0,
+        WindowClose,
+        WindowResize,
+        WindowFocus,
+        WindowLostFocus,
+        WindowMoved,
+        LoadResource,
+        FreeResource,
+        EngineTick,
+        EngineUpdate,
+        EngineRender,
+        KeyPressed,
+        KeyReleased,
+        MouseButtonPressed,
+        MouseButtonReleased,
+        MouseMoved,
+        MouseScrolled,
+        Custom
+    };
+
+    enum class EventCategory
+    {
+        None = 0,
+        Window,
+        Resource,
+        Engine,
+        Input,
+        Custom
+    };
+
+    class Event
+    {
+    private:
+        EventType m_type;
+        EventCategory m_category;
+        std::string m_name;
+
+    public:
+        Event();
+        Event(EventType const &);
+        virtual ~Event()
+        {
+        }
+
+        constexpr auto get_name() const -> char const *
+        {
+            if (std::empty(m_name)) {
+                return (this->get_name_from_type(m_type));
+            } else {
+                return (m_name.c_str());
+            }
+        }
+
+        constexpr auto get_type() const -> EventType
+        {
+            return (m_type);
+        }
+
+        constexpr auto get_category() const -> EventCategory
+        {
+            return (m_category);
+        }
+
+        virtual auto get_name_from_type(EventType const &type) const -> char const *;
+        virtual auto get_category_from_type(EventType const &type) const -> EventCategory;
+        virtual auto set_name(std::string const &name) -> void;
+
+        constexpr auto operator==(Event const &rhs) const -> bool
+        {
+            return (this->get_name() == rhs.get_name());
+        }
+
+        constexpr auto operator!=(Event const &rhs) const -> bool
+        {
+            return (!(*this == rhs));
+        }
+
+        constexpr auto operator<=>(Event const &rhs) const -> std::strong_ordering
+        {
+            return (this->get_type() <=> rhs.get_type());
+        }
+
+        auto to_string() const -> std::string;
+        friend inline auto operator<<(std::ostream &os, Event const &event) -> std::ostream &
+        {
+            return (os << event.to_string());
+        }
+    };
+}  // namespace event
+
+template <>
+struct fmt::formatter<event::Event> : formatter<std::string>
+{
+    auto format(event::Event const &event, format_context &ctx) const -> format_context::iterator
+    {
+        return formatter<std::string>::format(event.to_string(), ctx);
+    }
+};
