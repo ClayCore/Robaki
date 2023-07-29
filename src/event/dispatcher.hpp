48d758c30354b1a2c93601e1ddc07e1a8fa8a299
lunarium <claymorealt@op.pl>
2023-07-29 19:21:54 +0200

    Add event dispatcher class

---
+++ src/event/dispatcher.hpp
@@ -0,0 +1,54 @@
+/** @file dispatcher.hpp */
+
+#pragma once
+
+// module includes
+#include "event.hpp"
+#include "util/util.hpp"
+
+namespace event
+{
+    template <class Derived>
+    class Dispatcher
+    {
+    protected:
+        std::vector<Event> m_events;
+
+    public:
+        Dispatcher()
+        {
+            m_events = {};
+        }
+
+        auto derived() -> Derived *
+        {
+            return (static_cast<Derived *>(this));
+        }
+
+        auto derived() const -> Derived const *
+        {
+            return (static_cast<Derived const *>(this));
+        }
+
+        auto get_event(util::types::usize index) const -> Event
+        {
+            this->derived().get_event(index);
+        }
+
+        auto set_event(Event const &event, util::types::usize index) -> void
+        {
+            this->derived().set_event(event, index);
+        }
+
+        auto add_event(Event const &event) -> void
+        {
+            this->derived().add_event(event);
+        }
+
+        template <class Emitter>
+        auto dispatch(Event const &event, Emitter &emitter) -> void
+        {
+            this->derived().dispatch(event, emitter);
+        }
+    };
+}  // namespace event
