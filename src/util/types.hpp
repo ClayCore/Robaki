a5389b5c70abf8f428548f0bd956583118bf4d05
lunarium <claymorealt@op.pl>
2023-08-03 14:01:14 +0200

    Add utilities for creating shared and unique pointers

--- src/util/types.hpp
+++ src/util/types.hpp
@@ -31,7 +31,25 @@ namespace util::types
     using f64  = double;
     using f128 = long double;
 
-    template <class Derived>
+    template <typename T>
+    using Scope = std::unique_ptr<T>;
+
+    template <typename T, typename... Args>
+    constexpr auto CreateScope(Args &&...args) -> Scope<T>
+    {
+        return (std::make_unique<T>(std::forward<Args>(args)...));
+    }
+
+    template <typename T>
+    using Ref = std::shared_ptr<T>;
+
+    template <typename T, typename... Args>
+    constexpr auto CreateRef(Args &&...args) -> Ref<T>
+    {
+        return (std::make_shared<T>(std::forward<Args>(args)...));
+    }
+
+    template <typename Derived>
     class Singleton
     {
     private:
