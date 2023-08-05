01fdb69dd6c33f6b3b42d20296601fbc54231c08
lunarium <claymorealt@op.pl>
2023-08-05 14:51:13 +0200

    Add 2D and 3D `Vec` class to math module

--- src/util/math.hpp
+++ src/util/math.hpp
@@ -5,9 +5,27 @@
 #pragma once
 
 // module includes
+#include "traits.hpp"
 #include "types.hpp"
 
 namespace util::math
 {
-    // TODO
-}
+    template <typename T>
+    requires std::is_arithmetic_v<T>
+    struct Vec2
+    {
+        T x;
+        T y;
+    };
+
+    template <typename T>
+    requires std::is_arithmetic_v<T>
+    struct Vec3
+    {
+        T x;
+        T y;
+        T z;
+    };
+}  // namespace util::math
+
+using namespace util::math;
