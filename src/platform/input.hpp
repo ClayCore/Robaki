b1e828da28fa5c1c1df623e0c8efc3a9726b3616
lunarium <claymorealt@op.pl>
2023-08-05 16:51:46 +0200

    Replace all doubled f32s with Vectors

--- src/platform/input.hpp
+++ src/platform/input.hpp
@@ -14,26 +14,57 @@
 
 namespace platform
 {
+    /**
+     * Describes a single button and provides an event listener
+     * via the inherited `Subscriber` class
+     */
     struct Button : public event::Subscriber
     {
-        usize id;
-        std::string name;
+        // ================================================================================================ //
+        // Associated data ================================================================================ //
+        // ================================================================================================ //
 
-        bool down;
-        bool last, last_tick;
-        bool pressed, pressed_tick;
+        /** ID of the button (used by GLFW3) */
+        usize id = { 0 };
+
+        /** Refer to any button via name (also thanks to GLFW3) */
+        std::string name = { "None" };
+
+        bool down         = { false };
+        bool last         = { false };
+        bool last_tick    = { false };
+        bool pressed      = { false };
+        bool pressed_tick = { false };
+
+        // ================================================================================================ //
+        // Constructors and destructors =================================================================== //
+        // ================================================================================================ //
 
         Button() = default;
-        Button(usize id, std::string name) : id(id), name(std::move(name))
+        Button(usize p_id, std::string p_name) : id(p_id), name(std::move(p_name))
         {
         }
 
+        // ================================================================================================ //
+        // Button implementation methods ================================================================== //
+        // ================================================================================================ //
+
+        /**
+         * Toggle the button if it's not pressed anymore
+         *
+         * Called on each update
+         */
         constexpr auto update() -> void
         {
             pressed = down && !last;
             last    = down;
         }
 
+        /**
+         * Toggle the button if it's not pressed anymore
+         *
+         * Called on each tick
+         */
         constexpr auto tick() -> void
         {
             pressed_tick = down && !last_tick;
@@ -41,14 +72,29 @@ namespace platform
         }
     };
 
+    /**
+     * Describes any generic input device
+     *
+     * Inheritable for custom input devices.
+     */
     struct Input
     {
+        // ================================================================================================ //
+        // Constructors and destructors =================================================================== //
+        // ================================================================================================ //
         Input(const Input &)            = default;
         Input(Input &&)                 = delete;
         Input &operator=(const Input &) = default;
         Input &operator=(Input &&)      = delete;
         virtual ~Input()                = default;
 
+        // ================================================================================================ //
+        // Input implementation methods (for overriding) ================================================== //
+        // ================================================================================================ //
+
+        /**
+         * Update all the buttons for the device, called on each update
+         */
         virtual auto update() -> void
         {
             for (auto &button : this->get_buttons()) {
@@ -56,6 +102,9 @@ namespace platform
             }
         }
 
+        /**
+         * Tick all the buttons for the device, called on each tick
+         */
         virtual auto tick() -> void
         {
             for (auto &button : this->get_buttons()) {
@@ -63,6 +112,11 @@ namespace platform
             }
         }
 
+        /**
+         * Acquire an iterator to all `Button` derivatives.
+         *
+         * @return iterator
+         */
         virtual auto get_buttons() -> util::Iterator<Button *>
         {
             return (util::Iterator<Button *>{});
@@ -77,11 +131,9 @@ namespace platform
     struct Mouse : Input
     {
     private:
-        f32 xpos_last = { 0 };
-        f32 ypos_last = { 0 };
+        Vec2<f32> last;
 
-        f32 xpos_last_tick = { 0 };
-        f32 ypos_last_tick = { 0 };
+        Vec2<f32> last_tick;
 
         f32 last_scroll      = { 0 };
         f32 last_scroll_tick = { 0 };
@@ -104,20 +156,13 @@ namespace platform
         Mode mode;
         std::vector<Button> buttons;
 
-        f32 xpos            = { 0.0F };
-        f32 ypos            = { 0.0F };
-        f32 xpos_normalized = { 0.0F };
-        f32 ypos_normalized = { 0.0F };
+        Vec2<f32> pos;
+        Vec2<f32> pos_delta;
+        Vec2<f32> pos_norm;
+        Vec2<f32> pos_delta_norm;
 
-        f32 xpos_delta            = { 0.0F };
-        f32 ypos_delta            = { 0.0F };
-        f32 xpos_delta_normalized = { 0.0F };
-        f32 ypos_delta_normalized = { 0.0F };
-
-        f32 xpos_delta_tick            = { 0.0F };
-        f32 ypos_delta_tick            = { 0.0F };
-        f32 xpos_delta_tick_normalized = { 0.0F };
-        f32 ypos_delta_tick_normalized = { 0.0F };
+        Vec2<f32> pos_delta_tick;
+        Vec2<f32> pos_delta_norm_tick;
 
         f32 scroll            = { 0 };
         f32 scroll_delta      = { 0 };
@@ -138,14 +183,13 @@ namespace platform
         {
             Input::update();
 
-            xpos_delta            = xpos - xpos_last;
-            xpos_delta_normalized = xpos_delta / 800;
+            pos_delta      = pos - last;
+            pos_delta_norm = pos_delta / Vec2<i32>{ 800, 600 };
 
-            ypos_delta            = ypos - ypos_last;
-            ypos_delta_normalized = ypos_delta / 600;
+            last = pos;
 
-            xpos_last = xpos;
-            ypos_last = ypos;
+            scroll_delta = scroll - last_scroll;
+            last_scroll  = scroll;
 
             scroll_delta = scroll - last_scroll;
             last_scroll  = scroll;
@@ -155,14 +199,10 @@ namespace platform
         {
             Input::tick();
 
-            xpos_delta_tick            = xpos - xpos_last_tick;
-            xpos_delta_tick_normalized = xpos_delta_tick / 800;
+            pos_delta_tick      = pos - last_tick;
+            pos_delta_norm_tick = pos_delta_tick / Vec2<i32>{ 800, 600 };
 
-            ypos_delta_tick            = ypos - ypos_last_tick;
-            ypos_delta_tick_normalized = ypos_delta_tick / 600;
-
-            xpos_last_tick = xpos;
-            ypos_last_tick = ypos;
+            last_tick = pos;
 
             scroll_delta_tick = scroll - last_scroll_tick;
             last_scroll_tick  = scroll;
