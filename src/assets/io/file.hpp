d6f6ad2be158407307fdcb6ce8c7a78bc6842968
lunarium <claymorealt@op.pl>
2023-08-08 18:24:15 +0200

    Add stub for file I/O

---
+++ src/assets/io/file.hpp
@@ -0,0 +1,15 @@
+/** @file file.hpp */
+
+#pragma once
+
+namespace assets::io::file
+{
+    enum class FileFormat
+    {
+
+    };
+
+    class File
+    {
+    };
+}  // namespace assets::io::file
