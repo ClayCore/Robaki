8e0de4a337ad30a526faae4cccb84b1e12016927
lunarium <claymorealt@op.pl>
2023-08-18 18:30:28 +0200

    Add `macros` module to utilities header

--- src/util/util.hpp
+++ src/util/util.hpp
@@ -8,9 +8,11 @@
 #include "demangle.hpp"
 #include "iterator.hpp"
 #include "literals.hpp"
+#include "macros.hpp"
 #include "math.hpp"
 #include "string.hpp"
 #include "time.hpp"
 #include "traits.hpp"
 #include "types.hpp"
 #include "uuid.hpp"
+
