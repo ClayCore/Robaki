81f160f1bcaa858b0c05944a6e8dc36e9ac21fe6
lunarium <claymorealt@op.pl>
2023-08-19 17:51:13 +0200

    Add basic `FallbackAllocator` implementation

---
+++ src/memory/fallback_allocator.cpp
@@ -0,0 +1,46 @@
+/** @file fallback_allocator.cpp */
+
+// module includes
+#include "fallback_allocator.hpp"
+
+namespace memory
+{
+    template <class Primary, class Secondary>
+    auto FallbackAllocator<Primary, Secondary>::alloc(usize size) -> Block
+    {
+        auto block = m_primary.alloc(size);
+        if (block.addr != nullptr) {
+            return (block);
+        } else {
+            return (m_secondary.alloc(size));
+        }
+    }
+
+    template <class Primary, class Secondary>
+    auto FallbackAllocator<Primary, Secondary>::owns(Block &block) const noexcept -> bool
+    {
+        return (m_primary.owns(block) || m_secondary.owns(block));
+    }
+
+    template <class Primary, class Secondary>
+    auto FallbackAllocator<Primary, Secondary>::free(Block &block) -> void
+    {
+        if (m_primary.owns(block)) {
+            m_primary.free(block);
+        } else {
+            m_secondary.free(block);
+        }
+    }
+    template <class Primary, class Secondary>
+    inline auto FallbackAllocator<Primary, Secondary>::get_primary() noexcept -> Primary &
+    {
+        return (m_primary);
+    }
+
+    template <class Primary, class Secondary>
+    inline auto FallbackAllocator<Primary, Secondary>::get_secondary() noexcept -> Secondary &
+    {
+        return (m_secondary);
+    }
+
+}  // namespace memory
---
+++ src/memory/fallback_allocator.hpp
@@ -0,0 +1,29 @@
+/** @file fallback_allocator.hpp */
+
+#pragma once
+
+// module includes
+#include "allocator_interface.hpp"
+#include "block.hpp"
+#include "util/util.hpp"
+#include "utils.hpp"
+
+
+namespace memory
+{
+    template <class Primary, class Secondary>
+    class FallbackAllocator final : public AllocatorInterface
+    {
+    private:
+        Primary m_primary;
+        Secondary m_secondary;
+
+    public:
+        auto alloc(usize size) -> Block override;
+        auto owns(Block &block) const noexcept -> bool override;
+        auto free(Block &block) -> void override;
+
+        inline auto get_primary() noexcept -> Primary &;
+        inline auto get_secondary() noexcept -> Secondary &;
+    };
+}  // namespace memory
